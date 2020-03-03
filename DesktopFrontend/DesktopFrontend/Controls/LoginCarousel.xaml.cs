491502798798b5f11c2d8189ad00e00570970333
Drunkennesss <alal2001al@gmail.com>
2020-03-03 14:23:33 +0200

    renamed mainwindow into loginwindow\nadded caruosel\naddled temporary realization of Connection class

---
+++ DesktopFrontend/DesktopFrontend/Controls/LoginCarousel.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DesktopFrontend.Controls
+{
+    public class LoginCarousel : UserControl
+    {
+        private Carousel _carousel;
+        private Button _left;
+        private Button _right;
+        
+
+        public LoginCarousel()
+        {
+            this.InitializeComponent();
+            _left.Click += (s, e) => _carousel.Previous();
+            _right.Click += (s, e) => _carousel.Next();
+           
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            _carousel = this.FindControl<Carousel>("carousel");
+            _left = this.FindControl<Button>("left");
+            _right = this.FindControl<Button>("right");
+           
+        }        
+    }
+}
---
+++ DesktopFrontend/DesktopFrontend/ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesktopFrontend.Models;
+
+namespace DesktopFrontend.ViewModels
+{
+    public class LoginWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+
+        public void GetConnection()
+        {
+            Connection.Connect(null, 0);
+        }
+    }
+}
