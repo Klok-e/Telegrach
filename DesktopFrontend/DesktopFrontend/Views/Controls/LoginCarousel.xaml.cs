dd6482b9cb130c2522f53a467dfb2d4b51ab9b28
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-03-04 21:50:29 +0200

    add button disappear feature and make everything needlessly complicated

--- DesktopFrontend/DesktopFrontend/Views/Controls/LoginCarousel.xaml.cs
+++ DesktopFrontend/DesktopFrontend/Views/Controls/LoginCarousel.xaml.cs
@@ -1,13 +1,50 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
+using DynamicData.Binding;
 
 namespace DesktopFrontend.Views.Controls
 {
     public class LoginCarousel : UserControl
     {
+        public static readonly DirectProperty<LoginCarousel, bool> IsSelectedLastProperty =
+            AvaloniaProperty.RegisterDirect<LoginCarousel, bool>(nameof(IsSelectedLast),
+                lo => lo.IsSelectedLast);
+
+        private bool _isSelectedLast;
+
+        public bool IsSelectedLast
+        {
+            get => _isSelectedLast;
+            private set => SetAndRaise(IsSelectedLastProperty, ref _isSelectedLast, value);
+        }
+
+        public static readonly DirectProperty<LoginCarousel, bool> IsSelectedFirstProperty =
+            AvaloniaProperty.RegisterDirect<LoginCarousel, bool>(nameof(IsSelectedFirst),
+                lo => lo.IsSelectedFirst);
+
+        private bool _isSelectedFirst;
+
+        public bool IsSelectedFirst
+        {
+            get => _isSelectedFirst;
+            private set => SetAndRaise(IsSelectedFirstProperty, ref _isSelectedFirst, value);
+        }
+
+        private Carousel _carousel;
+
         public LoginCarousel()
         {
             InitializeComponent();
+            _carousel = this.FindControl<Carousel>("carousel");
+            _carousel.WhenPropertyChanged(c => c.SelectedIndex)
+                .Subscribe(_ =>
+                {
+                    IsSelectedLast = _carousel.SelectedIndex == _carousel.ItemCount - 1;
+                    IsSelectedFirst = _carousel.SelectedIndex == 0;
+                });
         }
 
         private void InitializeComponent()
