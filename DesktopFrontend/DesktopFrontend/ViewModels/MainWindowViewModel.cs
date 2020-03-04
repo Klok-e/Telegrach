271600a6fc4863e2ee0d5bd5de1016b6aa593c5f
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-03-04 15:06:43 +0200

    Move Login carousel to Views because it's a view

--- DesktopFrontend/DesktopFrontend/ViewModels/MainWindowViewModel.cs
+++ DesktopFrontend/DesktopFrontend/ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,12 @@ namespace DesktopFrontend.ViewModels
     class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Hello World!";
+        
+        public LoginWindowViewModel LoginViewModel { get; }
+
+        public MainWindowViewModel()
+        {
+            LoginViewModel=new LoginWindowViewModel();
+        }
     }
 }
