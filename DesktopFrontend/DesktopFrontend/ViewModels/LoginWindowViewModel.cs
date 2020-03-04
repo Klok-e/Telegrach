27c6b654beda882c54c4ed50b5e18b61fc27f7f3
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-03-04 15:00:14 +0200

    Make logging (Get connection) into a reactive command

--- DesktopFrontend/DesktopFrontend/ViewModels/LoginWindowViewModel.cs
+++ DesktopFrontend/DesktopFrontend/ViewModels/LoginWindowViewModel.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
 using DesktopFrontend.Models;
+using ReactiveUI;
 
 namespace DesktopFrontend.ViewModels
 {
     public class LoginWindowViewModel : ViewModelBase
     {
+        public ReactiveCommand<Unit,Unit> Login { get; }
         
+        public LoginWindowViewModel()
+        {
+            Login = ReactiveCommand.Create(() => { Connection.Connect(null, 0); });
+        }
 
-        public void GetConnection()
+        private void GetConnection()
         {
             Connection.Connect(null, 0);
         }
