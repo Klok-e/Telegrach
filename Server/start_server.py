c43d952f684e28d8ad91da7b1d89a4bae18c078b
sskcvqikfDESKTOP <antonapraks@gmail.com>
2020-03-05 09:45:06 +0200

    Simple TCP listener. Asyncio

--- Server/start_server.py
+++ Server/start_server.py
@@ -4,39 +4,62 @@ from typing import Tuple
 from config import *
 
 
-class Handler:
-    '''Handler for incoming connection'''
-    def __init__(self):
-        self._r = None
-        self._w = None
-        self._w_addr = None
+async def handle_read(reader: asyncio.StreamReader, sockname: Tuple[str, int]):
+    try:
+        data = await reader.readuntil(SEPARATOR)
+        print('read')
+        data = data.decode()
+        print(data)
+        commands, message = data.strip().split("\n\n")
+        print(f"Got request from {sockname}")
+        print(f"Command is {commands}")
+        print(f"Message if {message}")
+    except asyncio.LimitOverrunError:
+        print("LimitOverrunError")
 
-    async def create(self, reader, writer):
-        self._r = reader
-        self._w = writer
-        self._w_addr = self._w.get_extra_info("peername")
-        await self.handle_read()
-        await self.handle_write()
+    except asyncio.IncompleteReadError:
+        print("IncompleteReadError")
 
-    async def handle_read(self):
-        print(f"Reading from {self._w_addr}...")
-        data = await self._r.readuntil(SEPARATOR)
-        data = data.decode().strip()
-        comm, message = data.split("\n\n")
-        print(f"Got new command {comm} message {message} from {self._w_addr}")
-        # TODO: Handle asyncio.LimitOverrunError & asyncio.IncompleteReadError
+    except Exception as e:
+        print(str(e))
+        raise e
 
-    async def handle_write(self):
-        print(f"Writing to {self._w_addr}...")
-        self._w.write("message from server".encode())
-        await self._w.drain()
-        self._w.close()
+
+async def handle_write(writer: asyncio.StreamWriter, sockname: Tuple[str, int]):
+    print(f"Writing to {sockname}")
+    try:
+        writer.write("Message from server".encode())
+        await writer.drain()
+        print("Message sended...")
+    except:
+        print("handle_write Error")
+
+
+async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    sockname = writer.get_extra_info('peername')
+    print(f"New connection from {sockname}")
+
+    try:
+        await handle_read(reader, sockname)
+    except asyncio.LimitOverrunError:
+        print("LimitOverrunError")
+        
+    except asyncio.IncompleteReadError:
+        print("IncompleteReadError")
+
+    except Exception as e:
+        print(str(e))
+
+    try:
+        await handle_write(writer, sockname)
+    except:
+        print("TroubleShouting with writing")
+
+    writer.close()
 
 
 async def main():
-    server = await asyncio.start_server(Handler().create, *ADDRESS)
-
-    print(f"Serving on {ADDRESS}")
+    server = await asyncio.start_server(handler, *ADDRESS)
 
     async with server:
         await server.serve_forever()
