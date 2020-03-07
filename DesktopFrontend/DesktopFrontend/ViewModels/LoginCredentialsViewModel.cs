37ac959c0de2374ff4ae48fd8f804c4c67d928f0
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-03-07 19:33:58 +0200

    login credentials initial view and viewodel

--- DesktopFrontend/DesktopFrontend/ViewModels/LoginCredentialsViewModel.cs
+++ DesktopFrontend/DesktopFrontend/ViewModels/LoginCredentialsViewModel.cs
@@ -13,8 +13,35 @@ namespace DesktopFrontend.ViewModels
 {
     public class LoginCredentialsViewModel : ViewModelBase
     {
+        public ReactiveCommand<Unit, Unit> Back { get; }
+
+        public ReactiveCommand<Unit, bool> SignIn { get; }
+        
+        private string _login = "";
+
+        public string Login
+        {
+            get => _login;
+            set => this.RaiseAndSetIfChanged(ref _login, value);
+        }
+        
+        private string _password = "";
+
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+
         public LoginCredentialsViewModel(INavigationStack stack, IServerConnection connection)
         {
+            Back = ReactiveCommand.Create(() => { stack.Pop(); });
+            // TODO: make this into a real sign in
+            SignIn = ReactiveCommand.Create(() =>
+            {
+                stack.Push(new ChatViewModel());
+                return true;
+            });
         }
     }
 }
