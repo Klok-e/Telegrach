5a47dc2f97fa1c8980a6d7d6262f21aa37639b02
Code Formatter <runner@fv-az51.zoefv3lp2xuufmqiad5a4iz1cf.cx.internal.cloudapp.net>
2020-03-12 19:35:51 +0000

    Format code

--- DesktopFrontend/DesktopFrontend/Program.cs
+++ DesktopFrontend/DesktopFrontend/Program.cs
@@ -13,7 +13,7 @@ namespace DesktopFrontend
         // yet and stuff might break.
         public static void Main(string[] args) => BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
-    
+
         // Avalonia configuration, don't remove; also used by visual designer.
         private static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
