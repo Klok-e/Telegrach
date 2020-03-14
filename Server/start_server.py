d1edc6ea83ef7b8b24f0d0d143238da4cfa5b030
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-03-14 17:50:17 +0200

    check CI

--- Server/start_server.py
+++ Server/start_server.py
@@ -240,10 +240,10 @@ async def main():
     hostname = os.getenv("TELEGRACH_DB_HOST", None) or DB_HOST
     port = os.getenv("TELEGRACH_DB_PORT", None) or DB_PORT
     if any(
-        (us := not user,
-         pa := not password,
-         ho := not hostname,
-         po := not port)):
+            (us := not user,
+             pa := not password,
+             ho := not hostname,
+             po := not port)):
         for v in [
             x[1] for x in [
                 (us, "User"), (pa, "Password"), (ho, "DB Hostname"), (po, "DB Port")] if x[0]]:
