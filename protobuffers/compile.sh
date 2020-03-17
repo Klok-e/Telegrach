a2957de0c76487e15b7613a340089d6db2e60522
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-03-17 22:08:07 +0200

    compile.sh now removes output directories before compiling

--- protobuffers/compile.sh
+++ protobuffers/compile.sh
@@ -7,6 +7,9 @@ cd "${0%/*}"
 PYTHON_DIR=../Server/proto
 CSHARP_DIR=../DesktopFrontend/DesktopFrontend/Proto
 
+# remove old protofiles
+rm -rf $PYTHON_DIR $CSHARP_DIR
+
 mkdir -p $PYTHON_DIR $CSHARP_DIR
 
 protoc --csharp_out=$CSHARP_DIR --python_out=$PYTHON_DIR *.proto
