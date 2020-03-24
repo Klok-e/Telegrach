63393b1322498dd1c6b8fc539a9669c655f2b6e6
Alex <46966755+Drunkennesss@users.noreply.github.com>
2020-03-24 17:54:15 +0200

    Carousel pics (#32)
    * Added new logo and new carousel pics
    
    * made pic's comments visible/nslightly changed first pic

--- DesktopFrontend/DesktopFrontend/Views/MainWindow.xaml.cs
+++ DesktopFrontend/DesktopFrontend/Views/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace DesktopFrontend.Views
             // To fix a bug on linux where Height and Width are NaN and ListBox doesn't
             // want to show scrollbar until resize of the window
             var size = ClientSize;
-            Height = size.Height;
+            Height = size.Height + 50; //50 extra pixels makes pic's comments visible
             Width = size.Width;
         }
 
